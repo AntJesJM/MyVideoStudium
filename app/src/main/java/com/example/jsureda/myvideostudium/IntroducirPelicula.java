301baebbc148037a73050cffcb811a7a0e96b9ab
JSureda <jesussureda@gmail.com>
2018-01-29 19:38:08 +0100

    Inicializada GUI de la ventana de introducción de datos

--- app/src/main/java/com/example/jsureda/myvideostudium/IntroducirPelicula.java
+++ app/src/main/java/com/example/jsureda/myvideostudium/IntroducirPelicula.java
@@ -6,8 +6,18 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class IntroducirPelicula extends AppCompatActivity {
+
+    EditText txtTitulo, txtAnyo;
+    RatingBar ratBar;
+    Spinner spnGen;
+    ArrayAdapter<CharSequence> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,15 @@ public class IntroducirPelicula extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        inicializarUI();
+        adapter = ArrayAdapter.createFromResource(this, R.array.spinnerEstado, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnGen.setAdapter(adapter);
     }
-
+    public void inicializarUI(){
+        txtTitulo = (EditText) findViewById(R.id.txtTitulo2);
+        txtAnyo = (EditText) findViewById(R.id.txtAnyo2);
+        spnGen = (Spinner) findViewById(R.id.spnEstado2);
+        ratBar = (RatingBar) findViewById(R.id.barraNota);
+    }
 }
